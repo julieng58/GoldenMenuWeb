111b16ce07fdb38039934c962c68c7e5511c52e0
Julien <julien.gerard@live.fr>
2025-02-22 17:54:12 +0100

    Ajout copyright

--- init.js
+++ init.js
@@ -1,3 +1,20 @@
+/*
+ *  Copyright (c) 2025 Florian Nadin
+ *   All rights reserved.
+ *   Last modified 31/07/2022 18:11
+ *
+ *   Ce fichier fait partie du logiciel cédé conformément au contrat signé entre les parties.
+ *
+ *   Toute utilisation, modification ou distribution du code source est soumise aux conditions de la cession :
+ *   - Le logiciel est fourni en l'état, sans garantie d'aucune sorte.
+ *   - L'utilisation est restreinte à l'usage prévu par le cessionnaire.
+ *   - Toute reproduction ou commercialisation du code sans autorisation expresse est interdite.
+ *   - Le cessionnaire est tenu de préserver la confidentialité du code source.
+ *
+ *   Voir le fichier README.md pour plus de détails sur les conditions d'utilisation.
+ *
+ */
+
 if (typeof firebase === 'undefined') throw new Error('hosting/init-error: Firebase SDK not detected. You must include it before /__/firebase/init.js');
 firebase.initializeApp({
   "apiKey": "AIzaSyBshY0ECAR3WMy4g0AtgObFVqwVzVw_vLc",
--- js/menuPapier.js
+++ js/menuPapier.js
@@ -1,3 +1,20 @@
+/*
+ *  Copyright (c) 2025 Florian Nadin
+ *   All rights reserved.
+ *   Last modified 23/08/2022 14:07
+ *
+ *   Ce fichier fait partie du logiciel cédé conformément au contrat signé entre les parties.
+ *
+ *   Toute utilisation, modification ou distribution du code source est soumise aux conditions de la cession :
+ *   - Le logiciel est fourni en l'état, sans garantie d'aucune sorte.
+ *   - L'utilisation est restreinte à l'usage prévu par le cessionnaire.
+ *   - Toute reproduction ou commercialisation du code sans autorisation expresse est interdite.
+ *   - Le cessionnaire est tenu de préserver la confidentialité du code source.
+ *
+ *   Voir le fichier README.md pour plus de détails sur les conditions d'utilisation.
+ *
+ */
+
 var database = firebase.firestore();
 
 
